1e48399b4a8ea6a38aa8c1585056972260ae6167
dcastelar78byui <59662732+dcastelar78byui@users.noreply.github.com>
2023-04-21 19:56:08 -0300

    april

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        
+        // create number random
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,100);
+
+        int userNumber = -1;
+
+        while(userNumber != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (randomNumber > userNumber)
+            {
+                Console.WriteLine(" Higher");
+            }
+            else if (randomNumber < userNumber)
+            {
+                Console.WriteLine(" Lower");
+            }
+            else
+            {
+                Console.WriteLine(" You guessed it !!!");
+            }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,52 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int userIntN = -1;
+        string userResponse ="";
+        int sumList = 0;
+        float promList = 1f;
+        int maxList = 0;
+
+        while (userIntN != 0)
+        {
+            Console.Write("Enter a number (0 to quiet): ");
+            userResponse = Console.ReadLine();
+            userIntN = int.Parse(userResponse);
+
+            if (userIntN != 0)
+            {
+                numbers.Add(userIntN);
+            }
+                        
+        }
+        // sum
+        foreach(int number in numbers)
+        {
+            sumList = sumList + number;
+        }
+
+        // average
+        promList = ((float)sumList) / numbers.Count;
+        
+        //Max
+        foreach (int number in numbers)
+        {
+            if (number > maxList)
+            {
+                maxList = number;
+
+            }
+        }
+
+        Console.WriteLine($"The Sum is: {sumList} ");
+        Console.WriteLine($"The Average is: {promList} ");
+        Console.WriteLine($"The largest number is: {maxList} ");
+
+
+
+
+
     }
 }
